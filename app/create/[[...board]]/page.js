59ee9a78ed553c7c7cf8edd23308c4830ffbf78a
Anirudh Kadian <coderanirudh@gmail.com>
2025-01-11 19:33:58 +0530

    building the whiteboard

--- app/create/[[...board]]/page.js
+++ app/create/[[...board]]/page.js
@@ -1,7 +1,19 @@
+"use client";
 import React from "react";
 
-const page = () => {
-  return <div className="mt-20">page</div>;
+const Page = ({ params }) => {
+  const { board: boardId } = params; // Note: using 'story' since it's from [[...story]]
+
+  return (
+    <div className="mt-20">
+      <button
+        onClick={() => console.log(boardId)}
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+      >
+        Log Board ID
+      </button>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
